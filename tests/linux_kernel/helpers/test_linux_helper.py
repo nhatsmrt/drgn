9e41cf8f8e095a7474113fc53577495da420c895
Nhat Pham <nphamcs@gmail.com>
2022-08-14 18:15:16 -0700

    Add a test for the slab alloced cache case

--- tests/linux_kernel/helpers/test_linux_helper.py
+++ tests/linux_kernel/helpers/test_linux_helper.py
@@ -2,13 +2,26 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from drgn.helpers.linux import identify_address
-from tests.linux_kernel import LinuxKernelTestCase
+from tests.linux_kernel import (
+    LinuxKernelTestCase,
+    skip_unless_have_full_mm_support,
+    skip_unless_have_test_kmod,
+)
 
 
-class TestSlab(LinuxKernelTestCase):
+class TestLinuxHelpers(LinuxKernelTestCase):
     def test_identify_symbol(self):
         symbol = self.prog.symbol("jiffies")
-        self.assertEqual(identify_address(
-            self.prog, symbol.address),
-            '"jiffies_64+0"'
-        )
+        self.assertEqual(identify_address(self.prog, symbol.address), '"jiffies_64+0"')
+
+    @skip_unless_have_full_mm_support
+    @skip_unless_have_test_kmod
+    def test_identify_slab_cache(self):
+        objects = self.prog["drgn_test_slab_objects"]
+
+        if not self.prog["drgn_test_slob"]:
+            for obj in objects:
+                self.assertEqual(
+                    identify_address(self.prog, obj),
+                    '"drgn_test"',
+                )
