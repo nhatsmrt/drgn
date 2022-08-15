acfb482107f95ba886b5c509634fe322925665b5
Nhat Pham <nphamcs@gmail.com>
2022-08-14 17:04:33 -0700

    Simplify address comparing

--- tests/linux_kernel/helpers/test_slab.py
+++ tests/linux_kernel/helpers/test_slab.py
@@ -131,6 +131,6 @@ class TestSlab(LinuxKernelTestCase):
         if not self.prog["drgn_test_slob"]:
             for obj in objects:
                 self.assertEqual(
-                    slab_cache_containing(self.prog, obj.value_()),
+                    slab_cache_containing(self.prog, obj),
                     cache,
                 )
